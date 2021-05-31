904c93d0f57174f93a1b89955c6f1afab94f3dd5
ibook506 <ibook.databook@gmail.com>
2021-05-31 13:33:14 +0100

    Display User Profile

--- src/components/Pages/Dashboard.js
+++ src/components/Pages/Dashboard.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import {getUsers} from '../../api'
 
 
  const Dashboard = () =>{
+    const [users, setUsers] = useState([])
+
+    useEffect(()=>{
+        const fetchUser = async () =>{
+            const users = await getUsers()
+            setUsers(users)
+        }
+ 
+        fetchUser()
+ 
+     }, [])
+ 
+ console.log(users)
+ 
     return (
         <main className='container'>
             <section>
                 <h1>Dashboard Page</h1>
+                
+                    <table className="table table-striped mt-3">
+                    <thead>
+                      <tr>
+                        <th>Details</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        users.map(user => (
+                          <tr key={user._id}>
+                            <td>
+                              <Link to={`/profile/${user._id}`}>{user.name}</Link>
+                            </td>
+                            <td>
+                              {user.email}
+                            </td>
+                            <td>
+                              {user.address}
+                            </td>
+                            <td>
+                              <Link to={`/edit-profile/${user._id}`}>Edit</Link>
+                            </td>
+                          </tr>
+                        ))
+                      }
+                    </tbody>
+                  </table>
+                
             </section>
         
         </main>
--- src/components/Pages/Edit Profile/EditProfile.js
+++ src/components/Pages/Edit Profile/EditProfile.js
@@ -1,39 +1,50 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import {NavLink, useHistory, useRouteMatch} from 'react-router-dom'
 import './EditProfile.css'
+import {updateUser, getUserEditProfile} from '../../../api'
 
 const EditProfile = () =>{
+  const match = useRouteMatch()
+  const [user, setUser] = useState();
+  const history = useHistory()
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const userId = await getUserEditProfile(match.params.id)
+      setUser(userId)
+    }
+    fetchUser()
+  }, [match.params.id]);
+
+  const onSubmit = async (data) => {
+   await updateUser(data, match.params.id)
+    history.push("/")
+  }
+
+  
+
     return (
+       user ? (
         <>
         <div className='container edit-profile'>
-
-        
-  <div class="row justify-content-md-center">
-    
-    <div class="col-md-auto">
-      
-      <form className="card needs-validation" noValidate>
+  <div className="row justify-content-md-center">
+    <div className="col-md-auto">
+      <form onSubmit={onSubmit} className="card needs-validation" noValidate>
         <h3 className='text-center text-danger'>Update Profile Details</h3>
+        
         <input className="form-control form-control-lg" type="text" placeholder="Full Name" aria-label=".form-control-lg example" />
-        <input className="form-control form-control-lg" type="email" placeholder="E-mail" aria-label=".form-control-lg example" />
+        <input className="form-control form-control-lg" type="email"  placeholder="E-mail" aria-label=".form-control-lg example" />
 
         <input className="form-control form-control-lg" type="number" placeholder="Phone Number" aria-label=".form-control-lg example" />
 
-        <input className="form-control form-control-lg" type="address" placeholder="Contact Address " aria-label=".form-control-lg example" />
+        <input className="form-control form-control-lg" type="address"  placeholder="Contact Address " aria-label=".form-control-lg example" />
 
-        <div class="input-group mb-3">
-        <input type="file" class="form-control" id="inputGroupFile02" />
-  
-</div>
+        
 
-
-  
         <div className='btn-controls'>
-             <NavLink to='/' type="button" className="btn btn-secondary">Close</NavLink>
-             <NavLink to='/edit-profile'  type="button"  className="btn btn-success">Save Record </NavLink>
-             </div>
-
-             
+             <NavLink to={`/profile/${user._id}`} type="button" className="btn btn-secondary">Close</NavLink>
+             <button  type="submit"  className="btn btn-success">Save Record </button>
+             </div>   
 </form>
 
     </div>
@@ -44,7 +55,12 @@ const EditProfile = () =>{
         </div>
 
         </>
+
+      ): <div>Loading</div>
+        
+      
     )
+    
 }
 
 export default EditProfile
