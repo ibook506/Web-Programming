2f9ccfa50f0b64cdf23087fa8836970931edb2a8
Emmanuel Luka <emmanuelluka40@gmail.com>
2021-07-18 12:44:58 +0100

    adding contents to homepage

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -12,6 +12,31 @@ import {HomeSlide} from './HomeSlide'
                 <h1>HOME</h1>
                 <h2>Welcome to CSC 506 Web and Internet Programming</h2>
                 <HomeSlide />
+            <div class="media-body">
+                                <h3>Brief History of the Internet</h3>
+                                <p>As far back as the early stirrings of the Cold War, the concept of a network connecting
+                            computers was under development by both government and university researchers looking
+                            for a better means to communicate and share research. The military at the time relied in
+                            part on microwave transmission technology for communications. An unexpected attack on
+                            some of these towers demonstrated how susceptible the technology was to failure of even
+                            small portions of the transmission path. This led the military to seek a method of commu-
+                            nicating that could withstand attack. At the same time, university researchers were trying
+                            to share their work between campuses, and were struggling with similar problems when
+                            their transmissions suffered drops in signal. Parties from both groups ended up at the same
+                            conference with presentations, and decided to collaborate in order to further their work.
+                            At the time, computers were far from what we know them as today. A single computer
+                            was a large, immobile assortment of equipment that took up an entire room. Data entry was
+                            done by using punched cards of paper, or the newest method of the time, magnetic tapes.
+                            Interacting with the computer meant reserving time on the equipment and traveling to
+                            where it was. Most machines were owned by universities, large corporations, or government
+                            organizations due to the staffing demands, size, and cost to acquire and maintain them. The
+                            image below depicts the UNIVAC 1, a system used by the United States Census Bureau
+                            and other large organizations like universities.</p>
+
+
+
+
+                    </div>
                 
             </section>
         
